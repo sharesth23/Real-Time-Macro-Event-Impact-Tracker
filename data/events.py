232e5101cc8c49b585b9ae13e3b0a81a72ab5a19
Sharesth <sharesth444yt@gmail.com>
2025-12-22 23:59:54 +0530

    added events in py file

---
+++ data/events.py
@@ -0,0 +1,17 @@
+MACRO_EVENTS = [
+    {
+        "name": "US CPI",
+        "country": "United States",
+        "indicator": "CPI",
+    },
+    {
+        "name": "US NFP",
+        "country": "United States",
+        "indicator": "Non Farm Payrolls",
+    },
+    {
+        "name": "FOMC Rate Decision",
+        "country": "United States",
+        "indicator": "Interest Rate Decision",
+    }
+]
