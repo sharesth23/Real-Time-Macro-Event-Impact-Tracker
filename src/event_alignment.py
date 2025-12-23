9cebf532af7690f4d7499845514a1651ab87ee6d
Sharesth <sharesth444yt@gmail.com>
2025-12-23 15:04:27 +0530

    removed unwanted part of code

--- src/event_alignment.py
+++ src/event_alignment.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def align_event(prices, event_time, window_minutes=120):
-    """
-    Extract price window around macro event.
-    """
+    
     start = event_time - pd.Timedelta(minutes=window_minutes)
     end = event_time + pd.Timedelta(minutes=window_minutes)
 
