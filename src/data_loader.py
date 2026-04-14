7e0f906e6c5e6f1b1a16887744ab338cc562bcd4
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 18:20:22 +0530

    added code for loading data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,12 +1,12 @@
 import yfinance as yf
 import pandas as pd
 
-def load_intraday_prices(ticker, start, end, interval="5m"):
-    df = yf.download(
-        ticker,
-        start=start,
-        end=end,
-        interval=interval,
-        progress=False
-    )
-    return df["Close"].dropna()
+def load_data(asset: str, interval="1m", period="5d"):
+    """
+    Fetch real market data using yfinance
+
+    Args:
+        asset: ticker (SPY, BTC-USD, EURUSD=X)
+        interval: 1m, 5m, 1h, 1d
+        period: 1d, 5d, 1mo, etc.
+    """
