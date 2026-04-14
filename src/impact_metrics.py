3913a067c0dd584fd98b1ec140518500e6e1f8b1
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 18:30:58 +0530

    just upgraded the event volatility function

--- src/impact_metrics.py
+++ src/impact_metrics.py
@@ -10,3 +10,14 @@ def event_return(series: pd.Series):
         return np.nan
 
     return (series.iloc[-1] / series.iloc[0]) - 1
+
+def event_volatility(series: pd.Series):
+    """
+    Computes volatility (std of returns)
+    """
+    returns = series.pct_change().dropna()
+
+    if len(returns) == 0:
+        return np.nan
+
+    return returns.std()
