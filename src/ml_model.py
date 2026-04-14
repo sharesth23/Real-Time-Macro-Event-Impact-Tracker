ba703ed5519910d23d2890449963974c850c4b32
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 13:45:43 +0530

    added class impactmodel

--- src/ml_model.py
+++ src/ml_model.py
@@ -1,3 +1,20 @@
 import joblib
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+
+class ImpactModel:
+
+    def __init__(self):
+        self.model = RandomForestRegressor()
+
+    def train(self):
+        X = np.random.rand(100, 3)
+        y = np.random.rand(100)
+        self.model.fit(X, y)
+        joblib.dump(self.model, "models/model.pkl")
+
+    def load(self):
+        self.model = joblib.load("models/model.pkl")
+
+    def predict(self, features):
+        return float(self.model.predict([features])[0])
