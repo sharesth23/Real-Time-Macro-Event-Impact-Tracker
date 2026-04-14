982d5417da4cf670f2ba810816eea94291bb0c9b
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 18:29:22 +0530

    just upgraded the event return function

--- src/impact_metrics.py
+++ src/impact_metrics.py
@@ -1,12 +1,12 @@
 import numpy as np
+import pandas as pd
 
-def event_return(series):
+
+def event_return(series: pd.Series):
+    """
+    Computes cumulative return over event window
+    """
+    if len(series) < 2:
+        return np.nan
+
     return (series.iloc[-1] / series.iloc[0]) - 1
-
-
-def event_volatility(series):
-    return series.pct_change().std()
-
-
-def macro_surprise(actual, forecast):
-    return actual - forecast
