24b2c2de25413c15228d87083f4a5d5971ae4fc0
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 18:22:43 +0530

    updating code for this even alignment file

--- src/event_alignment.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def align_event(prices, event_time, window_minutes=120):
-    
-    start = event_time - pd.Timedelta(minutes=window_minutes)
-    end = event_time + pd.Timedelta(minutes=window_minutes)
-
-    return prices.loc[start:end]
