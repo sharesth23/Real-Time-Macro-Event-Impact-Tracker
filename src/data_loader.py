45f740ad3a780157bc6698ec463daec4992533f3
sharesth23 <sharesth444yt@gmail.com>
2026-04-14 18:21:35 +0530

    added code for loading realtime data using yfinance

--- src/data_loader.py
+++ src/data_loader.py
@@ -10,3 +10,18 @@ def load_data(asset: str, interval="1m", period="5d"):
         interval: 1m, 5m, 1h, 1d
         period: 1d, 5d, 1mo, etc.
     """
+
+    df = yf.download(
+        tickers=asset,
+        interval=interval,
+        period=period,
+        progress=False
+    )
+
+    if df.empty:
+        raise ValueError(f"No data found for {asset}")
+
+    df = df.reset_index()
+    df.rename(columns={"Close": "price"}, inplace=True)
+
+    return df[["Datetime" if "Datetime" in df.columns else "Date", "price"]]
