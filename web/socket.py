67d6591ba3fb8877a9a265c7f5f5af5fe9c6542d
sharesth23 <sharesth444yt@gmail.com>
2026-04-18 13:35:07 +0530

    added code for applying websocket

--- web/socket.py
+++ web/socket.py
@@ -2,3 +2,20 @@ import asyncio
 import websockets
 import json
 import yfinance as yf
+
+async def stream(websocket):
+    while True:
+        data = yf.download("SPY", interval="1m", period="1d").tail(1)
+
+        price = float(data["Close"].iloc[-1])
+
+        await websocket.send(json.dumps({"price": price}))
+
+        await asyncio.sleep(5)
+
+async def main():
+    async with websockets.serve(stream, "0.0.0.0", 8765):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
